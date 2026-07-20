705c814d2e48dec52c23a0896f0de43fd4c35680
sedopeo254-oss <sedopeo254@gmail.com>
2026-07-20 14:22:16 +0400

    Build bundled multiplayer test clients

--- travis/build.sh
+++ travis/build.sh
@@ -12,17 +12,24 @@ TARGET_OS=${TARGET_OS:-""}
 VCPKG_TRIPLET=${VCPKG_TRIPLET:-""}
 EXTRA_CFLAGS=${EXTRA_CFLAGS:-""}
 BUNDLED_FONT=""
+BUNDLED_CORE=${BUNDLED_CORE:-"0"}
+CORE_FLAG="--no-core=true"
+BUILD_TARGET="ygoprodll"
+if [[ "$BUNDLED_CORE" == "1" ]]; then
+	CORE_FLAG=""
+	BUILD_TARGET="ygopro"
+fi
 if [[ -f "NotoSansJP-Regular.otf" ]]; then
 	BUNDLED_FONT="--bundled-font=NotoSansJP-Regular.otf"
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
 	if [[ -z "${VS_GEN:-""}" ]]; then
-		./premake5 vs2017 $BUNDLED_FONT --no-core=true --oldwindows=true --sound=miniaudio,sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
-		msbuild.exe -m -p:Configuration=$BUILD_CONFIG -p:Platform=Win32 ./build/ygo.sln -t:ygoprodll -verbosity:minimal -p:EchoOff=true
+		./premake5 vs2017 $BUNDLED_FONT $CORE_FLAG --oldwindows=true --sound=miniaudio,sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
+		msbuild.exe -m -p:Configuration=$BUILD_CONFIG -p:Platform=Win32 ./build/ygo.sln -t:$BUILD_TARGET -verbosity:minimal -p:EchoOff=true
 	else
-		./premake5 $VS_GEN $BUNDLED_FONT --no-core=true --sound=miniaudio,sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
-		msbuild.exe -m -p:Configuration=$BUILD_CONFIG -p:Platform=Win32 ./build/ygo.sln -t:ygoprodll -verbosity:minimal -p:EchoOff=true
+		./premake5 $VS_GEN $BUNDLED_FONT $CORE_FLAG --sound=miniaudio,sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
+		msbuild.exe -m -p:Configuration=$BUILD_CONFIG -p:Platform=Win32 ./build/ygo.sln -t:$BUILD_TARGET -verbosity:minimal -p:EchoOff=true
 	fi
 	exit 0
 fi
@@ -49,7 +56,7 @@ fi
 if [[ "$TARGET_OS" == "ios" ]]; then
 	SOUND_BACKEND="miniaudio"
 fi
-./premake5 gmake2 $PREMAKE_FLAGS $BUNDLED_FONT --no-core=true --vcpkg-root=$VCPKG_ROOT --sound=$SOUND_BACKEND --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
+./premake5 gmake2 $PREMAKE_FLAGS $BUNDLED_FONT $CORE_FLAG --vcpkg-root=$VCPKG_ROOT --sound=$SOUND_BACKEND --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
 PROCS=""
 if [[ "$TRAVIS_OS_NAME" == "macosx" ]]; then
     PROCS=$(sysctl -n hw.ncpu)
@@ -64,8 +71,8 @@ if [[ "$TARGET_OS" == "windows" ]]; then
 	MAKE_FLAGS="${MAKE_FLAGS} RESCOMP=i686-w64-mingw32-windres"
 fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    make $MAKE_FLAGS -Cbuild -j$PROCS config="${BUILD_CONFIG}_${ARCH}" ygoprodll
+    make $MAKE_FLAGS -Cbuild -j$PROCS config="${BUILD_CONFIG}_${ARCH}" $BUILD_TARGET
 fi
 if [[ "$TRAVIS_OS_NAME" == "macosx" ]]; then
-    AR=ar make $MAKE_FLAGS -Cbuild -j$PROCS config="${BUILD_CONFIG}_${ARCH}" ygoprodll
+    AR=ar make $MAKE_FLAGS -Cbuild -j$PROCS config="${BUILD_CONFIG}_${ARCH}" $BUILD_TARGET
 fi
