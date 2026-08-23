dfdb124b965732ad4332029d3e205a554a1230de
sedopeo254-oss <sedopeo254@gmail.com>
2026-08-24 01:27:26 +0400

    Make Clockwork Night affect only current monsters

--- multiplayer-deck-master/c511000405.lua
+++ multiplayer-deck-master/c511000405.lua
@@ -1,6 +1,6 @@
 --機械仕掛けの夜－クロック・ワーク・ナイト－ (Anime)
 --Clockwork Night (Anime)
---Anime multiplayer: only opponents become Machines; controller +500 ATK, opponents -500 ATK.
+--Snapshot version: only monsters that are already on the field when this card resolves are affected.
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -8,25 +8,8 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--All opponent monsters become Machine, including monsters Summoned in Defense Position
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CHANGE_RACE)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(s.isopponent)
-	e2:SetValue(RACE_MACHINE)
-	c:RegisterEffect(e2)
-	--My monsters gain 500 ATK; opponent monsters lose 500 ATK
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_UPDATE_ATTACK)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e3:SetTarget(s.atktg)
-	e3:SetValue(s.atkval)
-	c:RegisterEffect(e3)
 end
 function s.logical(c)
 	local p=c:GetLogicalControler()
@@ -39,22 +22,64 @@ function s.is3v1()
 		and Duel.GetLogicalPlayerSide(2)==0
 		and Duel.GetLogicalPlayerSide(3)==1
 end
-function s.ismine(e,c)
-	return s.logical(c)==s.logical(e:GetHandler())
-end
-function s.isopponent(e,c)
-	local hp=s.logical(e:GetHandler())
-	local cp=s.logical(c)
-	if hp==nil or cp==nil or hp==cp then return false end
+function s.isopponent(owner,other)
+	if owner==nil or other==nil or owner==other then return false end
 	if s.is3v1() then
-		return (hp<3 and cp==3) or (hp==3 and cp<3)
+		return (owner<3 and other==3) or (owner==3 and other<3)
 	end
-	--Normal Duel and Battle Royale: every other logical player is an opponent.
-	return hp~=cp
+	return owner~=other
 end
-function s.atktg(e,c)
-	return s.ismine(e,c) or s.isopponent(e,c)
+function s.clockactive(e)
+	local sc=e:GetLabelObject()
+	return sc and sc:IsFaceup() and sc:IsLocation(LOCATION_SZONE) and not sc:IsDisabled()
 end
-function s.atkval(e,c)
-	return s.ismine(e,c) and 500 or -500
+function s.apply(sc,tc,atkchange,make_machine)
+	if make_machine then
+		local e1=Effect.CreateEffect(sc)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_RACE)
+		e1:SetCondition(s.clockactive)
+		e1:SetValue(RACE_MACHINE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		e1:SetLabelObject(sc)
+		tc:RegisterEffect(e1)
+	end
+	local e2=Effect.CreateEffect(sc)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetCondition(s.clockactive)
+	e2:SetValue(atkchange)
+	e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+	e2:SetLabelObject(sc)
+	tc:RegisterEffect(e2)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local sc=e:GetHandler()
+	if not sc:IsRelateToEffect(e) then return end
+	local mask=Duel.GetActiveLogicalPlayerMask()
+	if mask~=0 then
+		local owner=s.logical(sc)
+		for p=0,3 do
+			if mask&(1<<p)~=0 then
+				local g=Duel.GetPlayerFieldGroup(p,LOCATION_MZONE)
+				if p==owner then
+					for tc in aux.Next(g) do
+						s.apply(sc,tc,500,false)
+					end
+				elseif s.isopponent(owner,p) then
+					for tc in aux.Next(g) do
+						s.apply(sc,tc,-500,true)
+					end
+				end
+			end
+		end
+		return
+	end
+	--Normal 1v1 duel fallback.
+	for tc in aux.Next(Duel.GetFieldGroup(tp,LOCATION_MZONE,0)) do
+		s.apply(sc,tc,500,false)
+	end
+	for tc in aux.Next(Duel.GetFieldGroup(tp,0,LOCATION_MZONE)) do
+		s.apply(sc,tc,-500,true)
+	end
 end
