7a16fcb4562202c889bfea9beace5529d9e2e33d
sedopeo254-oss <sedopeo254@gmail.com>
2026-08-24 15:00:32 +0400

    Destroy Dice Dungeon when Orgoth the Relentless is destroyed

--- multiplayer-deck-master/c511001152.lua
+++ multiplayer-deck-master/c511001152.lua
@@ -16,20 +16,42 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_SELF_DESTROY)
 	e2:SetCondition(s.sdcon)
 	c:RegisterEffect(e2)
-	--Apply the chosen die results before damage calculation
+	--If either anime/alternate Orgoth the Relentless is destroyed, destroy Dice Dungeon.
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_ATKCHANGE)
-	e3:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e3:SetCondition(s.con)
-	e3:SetOperation(s.op)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCondition(s.orgothcon)
+	e3:SetOperation(s.orgothop)
 	c:RegisterEffect(e3)
+	--Apply the chosen die results before damage calculation
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_ATKCHANGE)
+	e4:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e4:SetCondition(s.con)
+	e4:SetOperation(s.op)
+	c:RegisterEffect(e4)
 end
 s.roll_dice=true
+s.listed_names={15744417,140000073}
 function s.sdcon(e)
 	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)==0
+end
+function s.orgothfilter(c)
+	local code=c:GetOriginalCode()
+	return code==15744417 or code==140000073
+end
+function s.orgothcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg and eg:IsExists(s.orgothfilter,1,nil)
+end
+function s.orgothop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsLocation(LOCATION_SZONE) then
+		Duel.Destroy(c,REASON_EFFECT)
+	end
 end
 function s.con(e,tp,eg,ep,ev,re,r,rp)
 	local at=Duel.GetAttacker()
