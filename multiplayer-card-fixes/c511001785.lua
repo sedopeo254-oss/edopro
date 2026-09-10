cf2bc64264ac2ba6724283cbf01997e5d9a09e66
sedopeo254-oss <sedopeo254@gmail.com>
2026-09-10 15:03:00 +0400

    Add selectable logical damage target to Spell of Pain

---
+++ multiplayer-card-fixes/c511001785.lua
@@ -0,0 +1,148 @@
+--Spell of Pain (Anime)
+--Multiplayer anime fix: the controller chooses which opposing logical player
+--receives damage that would be inflicted to them.
+local s,id=GetID()
+local MP_PLAYER_BASE=-68719476736 -- signed form of 0xfffffff000000000
+local MP_MAX_PLAYERS=26
+function s.initial_effect(c)
+	--effect damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(aux.damcon1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
+	c:RegisterEffect(e1)
+	--battle damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	c:RegisterEffect(e2)
+end
+function s.ismulti()
+	return Duel.GetActiveLogicalPlayerMask and Duel.GetActiveLogicalPlayerMask()~=0
+end
+function s.opponentmask(tp)
+	return Duel.GetLogicalPlayerMask(tp,false,false,true)
+end
+function s.selectplayer(tp,mask)
+	local players,options={},{}
+	for p=0,MP_MAX_PLAYERS-1 do
+		if (mask&(1<<p))~=0 and Duel.IsLogicalPlayerActive(p) then
+			players[#players+1]=p
+			options[#options+1]=MP_PLAYER_BASE+p
+		end
+	end
+	if #players==0 then return nil end
+	if #players==1 then return players[1] end
+	local op=Duel.SelectOption(tp,table.unpack(options))
+	return players[op+1]
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if not s.ismulti() then
+		if chk==0 then return true end
+		return
+	end
+	local mask=s.opponentmask(tp)
+	if chk==0 then return mask~=0 end
+	local logical=s.selectplayer(tp,mask)
+	e:SetLabel(logical or -1)
+end
+function s.refcon(e,re,val,r,rp,rc)
+	if (r&REASON_EFFECT)==0 then return false end
+	local cc=Duel.GetCurrentChain()
+	if cc==0 then return false end
+	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	return cid==e:GetLabel()
+end
+function s.capture(e,re,val,r,rp,rc)
+	if (r&REASON_EFFECT)==0 then return val end
+	local cid,target,total=e:GetLabel()
+	local cc=Duel.GetCurrentChain()
+	if cc==0 then return val end
+	local now=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	if now~=cid then return val end
+	e:SetLabel(cid,target,total+val)
+	return 0
+end
+function s.transfer(e,tp,eg,ep,ev,re,r,rp)
+	local ce=e:GetLabelObject()
+	if not ce then return end
+	local cid,target,total=ce:GetLabel()
+	if not target or target<0 or not total or total<=0 then return end
+	ce:SetLabel(cid,target,0)
+	if Duel.IsLogicalPlayerActive(target) then
+		Duel.DamagePlayer(target,total,REASON_EFFECT)
+	end
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	if not s.ismulti() then
+		local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_REFLECT_DAMAGE)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetTargetRange(1,0)
+		e1:SetLabel(cid)
+		e1:SetValue(s.refcon)
+		e1:SetReset(RESET_CHAIN)
+		Duel.RegisterEffect(e1,tp)
+		return
+	end
+	local target=e:GetLabel()
+	if target==nil or target<0 then return end
+	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
+	local ce=Effect.CreateEffect(e:GetHandler())
+	ce:SetType(EFFECT_TYPE_FIELD)
+	ce:SetCode(EFFECT_CHANGE_DAMAGE)
+	ce:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	ce:SetTargetRange(1,0)
+	ce:SetLabel(cid,target,0)
+	ce:SetValue(s.capture)
+	ce:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(ce,tp)
+	local te=Effect.CreateEffect(e:GetHandler())
+	te:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	te:SetCode(EVENT_CHAIN_SOLVED)
+	te:SetLabelObject(ce)
+	te:SetOperation(s.transfer)
+	te:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(te,tp)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetBattleDamage(tp)>0
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if not s.ismulti() then
+		if chk==0 then return true end
+		return
+	end
+	local mask=s.opponentmask(tp)
+	if chk==0 then return mask~=0 end
+	local logical=s.selectplayer(tp,mask)
+	e:SetLabel(logical or -1)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	if not s.ismulti() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetTargetRange(1,0)
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		Duel.RegisterEffect(e1,tp)
+		return
+	end
+	local target=e:GetLabel()
+	local dam=Duel.GetBattleDamage(tp)
+	if target==nil or target<0 or dam<=0 then return end
+	Duel.ChangeBattleDamage(tp,0)
+	if Duel.IsLogicalPlayerActive(target) then
+		Duel.DamagePlayer(target,dam,REASON_EFFECT)
+	end
+end
