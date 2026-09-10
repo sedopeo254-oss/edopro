b213b7b4567a6c497e766c4dc2be12e02ef9ff1f
sedopeo254-oss <sedopeo254@gmail.com>
2026-09-10 15:02:28 +0400

    Let Seven Tools choose any active Trap link

---
+++ multiplayer-card-fixes/c3819470.lua
@@ -0,0 +1,65 @@
+--盗賊の七つ道具
+--Seven Tools of the Bandit
+--Anime multiplayer fix: if several Trap activations are on the current Chain,
+--the player chooses which one to negate and destroy.
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetCost(Cost.PayLP(1000))
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsTrapEffect() and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
+end
+function s.gettraps(e)
+	local g=Group.CreateGroup()
+	local cc=Duel.GetCurrentChain()
+	for i=1,cc do
+		local te=Duel.GetChainInfo(i,CHAININFO_TRIGGERING_EFFECT)
+		local tc=te and te:GetHandler() or nil
+		if te and tc and tc~=e:GetHandler() and te:IsTrapEffect()
+			and te:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(i) then
+			g:AddCard(tc)
+		end
+	end
+	return g
+end
+function s.findchain(tc)
+	for i=Duel.GetCurrentChain(),1,-1 do
+		local te=Duel.GetChainInfo(i,CHAININFO_TRIGGERING_EFFECT)
+		if te and te:GetHandler()==tc and te:IsTrapEffect()
+			and te:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(i) then
+			return i
+		end
+	end
+	return 0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=s.gettraps(e)
+	if chk==0 then return g:GetCount()>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+	local sg=g:Select(tp,1,1,nil)
+	local tc=sg:GetFirst()
+	local ch=tc and s.findchain(tc) or 0
+	e:SetLabel(ch)
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,sg,1,0,0)
+	if tc and tc:IsDestructable() then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,1,0,0)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local ch=e:GetLabel()
+	if ch<=0 then return end
+	local te=Duel.GetChainInfo(ch,CHAININFO_TRIGGERING_EFFECT)
+	local tc=te and te:GetHandler() or nil
+	if not te or not tc then return end
+	if Duel.NegateActivation(ch) and tc:IsRelateToEffect(te) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
